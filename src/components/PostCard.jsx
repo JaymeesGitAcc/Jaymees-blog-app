066f566079e93edccaa15f78eb043431fc400809
Andrew James <mygithubacc3198@gmail.com>
2024-04-02 20:44:28 +0530

    Added LazyLoadImage component

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 import appwriteService from "../appwrite/config.js";
 import { FaArrowRight, FaUser } from "react-icons/fa";
 import { FaCalendarAlt } from "react-icons/fa";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 function PostCard({
     $id,
     $createdAt,
     title,
     featuredImage,
+    genre,
     author,
     className = "",
     fontSize = "text-md",
@@ -20,18 +22,21 @@ function PostCard({
     }/${dateObj.getFullYear()}`;
 
     return (
-        <article
-            className={`w-full bg-white overflow-hidden shadow-lg shadow-slate-300 ${className}`}
-        >
+        <article className={`w-full bg-white overflow-hidden ${className}`}>
             <Link to={`/post/${$id}`}>
                 <div className="group relative w-full h-full overflow-hidden after:absolute after:duration-300 after:inset-0 after:bg-black after:opacity-40 hover:after:opacity-35">
-                    <img
+                    <div
+                        className={`absolute z-20 py-1 px-2 rounded-md text-white ${fontSize} bg-[#9e33ff] top-2 left-2`}
+                    >
+                        <p className="text-[0.75em] font-semibold">{genre}</p>
+                    </div>
+                    <LazyLoadImage
                         src={appwriteService.getFilePreview(featuredImage)}
                         alt={title}
                         className="h-full w-full object-cover duration-300 scale-105 group-hover:scale-100"
                     />
                     <div
-                        className={`absolute bottom-4 left-4 right-2 text-slate-100 font-semibold z-20 text-md ${fontSize}`}
+                        className={`absolute bottom-4 left-4 right-2 text-slate-100 font-semibold z-20 ${fontSize}`}
                     >
                         <div className={`text-white my-2`}>
                             <h1 className="text-[1em] font-semibold">
